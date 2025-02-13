c31da5ba0aa83f21ac00d878ec0956942a455599
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2025-02-13 18:10:06 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab.js";
 import initModal from "./modules/modal.js"
+import initTooltip from "./modules/tooltip.js";
 initScrollSuave(); 
 initAnimacaoScroll(); 
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
 
 
 
