35c42f1414e6aca92f38c13f20a095287d093812
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2025-02-21 15:44:38 -0300

    fetch de bitcoin

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,22 @@
+export default function initFetchBitcoin (){
+  
+}
+
+
+
+async function fetchBitcoin(){
+  try{
+    const bitcoinResponse = await fetch('https://blockchain.info/ticker'); 
+    const bitcoinJSON = await bitcoinResponse.json(); 
+    const btcPreco = document.querySelector('.btc-preco');
+    if(btcPreco && bitcoinJSON){
+      btcPreco.innerText = (1000 / bitcoinJSON.BRL.sell).toFixed(4); 
+    }else{
+      console.log('Erro ao buscar o preço do bitcoin');
+    }
+  } catch(erro){
+     console.log(erro);
+  }
+}
+
+fetchBitcoin();
