a25d6b3108ba7163fe8d1150e7536f456e07c43b
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2025-02-24 18:06:52 -0300

    uma tentitva de uso de webpack

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // 'no-console': 'error'
+      indent: ['error', 2],
+      'keyword-spacing': 'error',
+      'no-multiple-empty-lines': 'error',
+      'eol-last': ['error', 'always'],
+      semi: ['error', 'always'],
+      'no-trailing-spaces': 'error',
+      'operator-assignment': 'error',
+      'no-inner-declarations': [
+        'error',
+        'functions',
+        { blockScopedFunctions: 'disallow' },
+      ],
+    },
+  },
 ];
--- js/script.js
+++ js/script.js
@@ -2,28 +2,21 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab.js";
-import initModal from "./modules/modal.js"
+import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
-import initDropdownMenu from "./modules/dropdown-menu.js"; 
-import initMenuMobile from "./modules/menu-mobile.js"; 
-import initAnimaNumeros from "./modules/anima-numeros.js"; 
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
-initScrollSuave(); 
-initAnimacaoScroll(); 
+initScrollSuave();
+initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
-initMenuMobile(); 
-initAnimaNumeros(); 
-initFuncionamento(); 
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
 initFetchBitcoin();
-
-
-
-
-
-
-
